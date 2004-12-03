18b845da8a4d0fe5cce4ed710e43199dc9b6695e
Olaf Klischat <olaf.klischat@googlemail.com>
2004-12-03 02:43:55 +0000

    first simple writing tests

--- test/xpath_test.rb
+++ test/xpath_test.rb
@@ -71,5 +71,21 @@ class XPathTest < Test::Unit::TestCase
   end
 
 
+  def test_write_noop
+    assert_equal @d.root.elements[1], XML::XPath.new("foo").first(@d.root,true)
+    assert_equal @d.root.elements[3].elements[1], XML::XPath.new("foo[2]/u").first(@d.root,true)
+    # TODO: deep-compare of REXML documents?
+  end
+
+  def test_write_byname_then_name
+    s1 = @d.elements[1].elements.size
+    s2 = @d.elements[1].elements[1].elements.size
+    node = XML::XPath.new("foo/new1").first(@d.root,true)
+    assert_equal "new1", node.name
+    assert node.attributes.empty?
+    assert_equal @d.elements[1].elements[1].elements[1], node
+    assert_equal s1, @d.elements[1].elements.size
+    assert_equal s2+1, @d.elements[1].elements[1].elements.size
+  end
 
 end
